c44b02eb2b4c0cdc7b6bca3c924b6db00fa0bd63
oanh2929 <oanhntk.24ite@vku.udn.vn>
2025-02-20 10:40:37 +0700

    code

--- src/Bai3_7/TestMain.java
+++ src/Bai3_7/TestMain.java
@@ -1,4 +1,17 @@
 package Bai3_7;
 
 public class TestMain {
+    public static void main(String[] args) {
+        Ball ball = new Ball(50, 50, 5, 2, 3);
+        Player player = new Player(7, 40, 40, 20, 20);
+
+        for (int i = 0; i < 10; i++) {
+            ball.move();
+            if (player.collidesWith(ball)) {
+                ball.reflectHorizontal();
+                ball.reflectVertical();
+            }
+            System.out.println(ball);
+        }
+    }
 }
